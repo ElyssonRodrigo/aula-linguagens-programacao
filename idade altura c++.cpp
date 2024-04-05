442c393a2724571a6e5ce518330d6dde8318d1ae
ElyssonRodrigo <elyssonmartins4@gmail.com>
2024-04-05 20:27:47 -0300

    idade altura c++

---
+++ idade altura c++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int idade;
+    float altura;
+    cout << "digite sua idade: "<< endl;
+    cin >> idade;
+    cout << " digite sua altura: "<< endl;
+    cin >> altura;
+    cout << "sua altura e "<< altura << " e voce tem "<< idade << " anos!" << endl;
+    return 0;
+}
